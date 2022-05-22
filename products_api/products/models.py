f8e380bee766bb218a34e69a83e63827f106d53f
Karthik <rajakarthik131@gmail.com>
2022-05-22 20:40:24 +0530

    Add field active in Product model

--- products_api/products/models.py
+++ products_api/products/models.py
@@ -6,3 +6,4 @@ class Product(TimeStampedModel):
     name = models.CharField("Product name", max_length=255)
     sku = models.CharField("SKU", max_length=300, unique=True)
     description = models.TextField(null=True, blank=True)
+    active = models.BooleanField("Active")
